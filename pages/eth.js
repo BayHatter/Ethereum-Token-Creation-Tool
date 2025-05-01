58a2715f5cc48e89144b3b9d61a4683568626245
BayHatter <bayhatter@gmail.com>
2025-05-01 18:46:35 +0500

    Update and rename pol.js to eth.js

--- pages/eth.js
+++ pages/eth.js
@@ -1,15 +1,15 @@
 import dynamic from 'next/dynamic';
 import WalletProvider from '../assets/WalletProvider';
 import TopMenu from '../assets/TopMenu';
-import Polygon from '../assets/Polygon';
+import Ethereum from '../assets/Ethereum';
 import Sidebar from '../assets/Sidebar';
 import Head from 'next/head';
 
-const PolygonWithNoSSR = dynamic(() => import('../assets/Polygon'), {
+const EthereumWithNoSSR = dynamic(() => import('../assets/Ethereum'), {
   ssr: false,
 });
 
-export default function Pol() {
+export default function Eth() {
   return (
     <div className="min-h-screen bg-white-100">
       <WalletProvider>
@@ -18,17 +18,17 @@ export default function Pol() {
             <TopMenu account={account} connectWallet={connectWallet} disconnectWallet={disconnectWallet} />
             
             <Head>
-              <title>Polygon Token Creation Tool</title>
-              <meta name="description" content="Create and launch your own meme token on Polygon" />
-              <meta property="og:title" content="Launch Your Meme Coin on Polygon" />
-              <meta property="og:description" content="Build and deploy a meme coin instantly on Polygon. Fast, secure, and 100% code-free." />
+              <title>Ethereum Token Creation Tool</title>
+              <meta name="description" content="Create and launch your own meme token on Ethereum" />
+              <meta property="og:title" content="Launch Your Meme Coin on Ethereum" />
+              <meta property="og:description" content="Build and deploy a meme coin instantly on Ethereum. Fast, secure, and 100% code-free." />
               <meta property="og:type" content="website" />
-              <meta property="og:url" content="/pol" />
+              <meta property="og:url" content="/eth" />
             </Head>
 
             <div className="container mx-auto p-4 max-w-7xl flex flex-col md:flex-row gap-4">
               <div className="flex-1">
-                <PolygonWithNoSSR provider={provider} account={account} connectWallet={connectWallet} />
+                <EthereumWithNoSSR provider={provider} account={account} connectWallet={connectWallet} />
               </div>
               <div className="md:w-[300px] flex-shrink-0">
                 <Sidebar />
